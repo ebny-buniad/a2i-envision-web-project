2f74b8f551adb65f7d288c807a7196c7637a6af2
ebny-buniad <ebnybuniad@gmail.com>
2024-03-17 15:54:02 +0600

    add service slider

--- asset/js/main.js
+++ asset/js/main.js
@@ -16,3 +16,34 @@ $(document).ready(function () {
 
 });
 
+$(document).ready(function (){
+
+});
+
+$(document).ready(function (){
+    $('.owl-carousel').owlCarousel({
+        dots: false,
+        items:4,
+        loop:false,
+        center:true,
+        // left:100,
+        margin:15,
+        URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: 'URLHash',
+        nav: true,
+        navText: ["<i class='far fa-arrow-alt-circle-left w'></i>","<i class='far fa-arrow-alt-circle-right'></i>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+});
+
